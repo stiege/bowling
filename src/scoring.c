017348c34c68f1562df5245c84e2945bf21fb6fd
alexho <alex.hodge@trutest.co.nz>
2014-12-12 09:55:35 +1300

    Source comments.

--- src/scoring.c
+++ src/scoring.c
@@ -2,33 +2,73 @@
 #include "stdio.h" //strings
 #include "stdbool.h" //bool
 
-#define SCORECARD_FRAME_SLOTS 10
-#define SCORE_ROLL_SLOTS 20
-#define SIZE_OF_ROLL_SLOTS 4
-#define SIZE_OF_FRAME 8
 
-#define FIRST_ELEMENT 1
+/*
+_________                _____.__
+\_   ___ \  ____   _____/ ____\__| ____
+/    \  \/ /  _ \ /    \   __\|  |/ ___\
+\     \___(  <_> )   |  \  |  |  / /_/  >
+ \______  /\____/|___|  /__|  |__\___  /
+        \/            \/        /_____/
+*/
+
+#define SCORECARD_FRAME_SLOTS 10
+/*
+"|   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |"
+"|   1   |   2   |   3   |   4   |   5   |   6   |   7   |   8   |   9   |   10  |"
+*/
+#define SCORE_ROLL_SLOTS 20
+/*
+"| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |   |   |   |   |   |   |   |   |   |   |20 |"
+"|       |       |       |       |       |       |       |       |       |       |";
+*/
+#define SIZE_OF_ROLL_SLOTS 4
+/*
+|   |   // <- last '|' belongs to the next row
+*/
+#define SIZE_OF_FRAME 8
+/*
+|       |    // <- last '|' belongs to the next frame
+*/
+
+/* 
+We have numbered everything above from 1 
+*/
+#define FIRST_ELEMENT 1 
 #define ROLL_FIRST_ELEMENT FIRST_ELEMENT
 #define FRAME_FIRST_ELEMENT FIRST_ELEMENT
-#define CARD_FIRST_ELEMENT FIRST_ELEMENT
-#define ROLLS_PER_FRAME 2
 
+#define ROLLS_PER_FRAME 2
+/*
+| 1 | 2 |
+|   1   |
+*/
+
+
+/*
+________          __           ________                .__
+\______ \ _____ _/  |______    \______ \   ____   ____ |  |
+ |    |  \\__  \\   __\__  \    |    |  \_/ __ \_/ ___\|  |
+ |    `   \/ __ \|  |  / __ \_  |    `   \  ___/\  \___|  |__
+/_______  (____  /__| (____  / /_______  /\___  >\___  >____/
+        \/     \/          \/          \/     \/     \/
+*/
 static struct tScoreCardState
 {
     int rollInFrame;
     int frameNumber;
-    int lastResult;
+    int lastRowScore;
     int lastFrameScore;
-    int thisScore;
+    int thisRowScore;
 }scoreCardState;
 
 static const struct tScoreCardState cardInitState = 
 {
     .rollInFrame = ROLL_FIRST_ELEMENT,
     .frameNumber = FRAME_FIRST_ELEMENT,
-    .lastResult = 0,
+    .lastRowScore = 0,
     .lastFrameScore = 0,
-    .thisScore = 0
+    .thisRowScore = 0
 };
 
 static const char blankCard[] = 
@@ -36,6 +76,15 @@ static const char blankCard[] =
 "|       |       |       |       |       |       |       |       |       |       |\n";
 
 static char currentCard[sizeof(blankCard)];
+
+/*
+__________                __
+\______   \_______  _____/  |_  ____  ______
+ |     ___/\_  __ \/  _ \   __\/  _ \/  ___/
+ |    |     |  | \(  <_> )  | (  <_> )___ \
+ |____|     |__|   \____/|__|  \____/____  >
+                                         \/
+*/
 
 static void resetCard(void);
 static void progressToNextFrame(void);
@@ -46,6 +95,14 @@ static char pinsToChar(int val);
 static int getReportCardRollOffset(void);
 static int getReportCardFrameResultOffset(void);
 
+/*
+   _____ __________.___
+  /  _  \\______   \   |
+ /  /_\  \|     ___/   |
+/    |    \    |   |   |
+\____|__  /____|   |___|
+        \/
+*/
 void SCRNG_Init( void )
 {
     resetCard();
@@ -68,9 +125,18 @@ void SCRNG_Roll(int pins)
         progressToNextFrame();
     }
 
-    scoreCardState.lastResult = scoreCardState.thisScore;
+    scoreCardState.lastRowScore = scoreCardState.thisRowScore;
 
 }
+
+/*
+  ___ ___         .__
+ /   |   \   ____ |  | ______   ___________  ______
+/    ~    \_/ __ \|  | \____ \_/ __ \_  __ \/  ___/
+\    Y    /\  ___/|  |_|  |_> >  ___/|  | \/\___ \
+ \___|_  /  \___  >____/   __/ \___  >__|  /____  >
+       \/       \/     |__|        \/           \/
+*/
 
 static void progressToNextFrame(void)
 {
@@ -78,8 +144,8 @@ static void progressToNextFrame(void)
     scoreCardState.rollInFrame = ROLL_FIRST_ELEMENT;
 
     scoreCardState.lastFrameScore = 
-        scoreCardState.lastResult
-        + scoreCardState.thisScore;
+        scoreCardState.lastRowScore
+        + scoreCardState.thisRowScore;
 }
 
 static bool frameIsComplete(void)
@@ -89,8 +155,8 @@ static bool frameIsComplete(void)
 
 static void markScoreCardForRoll(int pins)
 {
-    scoreCardState.thisScore = pins;
-    currentCard[getReportCardRollOffset()] = pinsToChar(scoreCardState.thisScore);
+    scoreCardState.thisRowScore = pins;
+    currentCard[getReportCardRollOffset()] = pinsToChar(scoreCardState.thisRowScore);
 }
 
 static void resetCard(void)
@@ -118,8 +184,8 @@ static int getReportCardRollOffset()
 
 static void markScoreCardForFrameResult(void)
 {
-    int frameScore = scoreCardState.thisScore 
-        + scoreCardState.lastResult
+    int frameScore = scoreCardState.thisRowScore 
+        + scoreCardState.lastRowScore
         + scoreCardState.lastFrameScore;
     currentCard[getReportCardFrameResultOffset()] = pinsToChar(frameScore);
 }
